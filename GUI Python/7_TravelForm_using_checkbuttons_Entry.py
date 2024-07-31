8aa5ea4530620e3ce694b6029dadf9ebe0112526
Uditya Narayan Tiwari <uditmerit@gmail.com>
2024-08-01 00:29:53 +0530

    Adding the file handling system

--- GUI Python/7_TravelForm_using_checkbuttons_Entry.py
+++ GUI Python/7_TravelForm_using_checkbuttons_Entry.py
@@ -1,6 +1,22 @@
 from tkinter import *
 
+
+
+        
 root  = Tk()
+
+def getvalue():
+        print("Form submission succesful")
+
+        print(f"{namevalue.get(), Phonevalue.get(), gendervalue.get(), emergencyvalue.get(), paymentvalue.get()}")
+
+        with open('7.1_record.text','a') as f:
+                f.write(f"{namevalue.get(), Phonevalue.get(), gendervalue.get(), emergencyvalue.get(), paymentvalue.get()}\n")
+
+
+root.title("My first form")
+icon = PhotoImage(file="books.png")
+root.iconphoto(True,icon)
 
 root.geometry("744x444")
 
@@ -15,8 +31,8 @@ emergency  = Label(root, text="Emergency Contact")
 payment = Label(root, text="Payment Mode")
 
 # packing of all the labels
-Phone.grid(row=1, column = 2)
-name.grid(row=2, column = 2)
+name.grid(row=1, column = 2)
+Phone.grid(row=2, column = 2)
 gender.grid(row=3, column = 2)
 emergency.grid(row=4, column = 2)
 payment.grid(row=5, column = 2)
@@ -36,12 +52,18 @@ genderentry = Entry(root, textvariable=gendervalue)
 emergencyentry = Entry(root, textvariable=emergencyvalue)
 paymententry = Entry(root, textvariable=paymentvalue)
 
-# packing all the elements of the entres
+# packing all the elements of the entries
 nameentry.grid(row=1, column=3)
 Phoneentry.grid(row=2, column=3)
 genderentry.grid(row=3, column=3)
 emergencyentry.grid(row=4, column=3)
 paymententry.grid(row=5, column=3)
 
+# checkbox and packing it
+foodservice = Checkbutton(text="Want to prebook your meals?", variable= foodservicevalue)
+foodservice.grid(row=6, column=3, pady=5)
+
+# Button & packing it
+Button(text="Sumbmit the form", command=getvalue).grid(row=7,column=3)
 
 root.mainloop()
